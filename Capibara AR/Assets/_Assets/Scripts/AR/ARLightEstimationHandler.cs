fd8f6ce057d76cbc75709b1b36ee2ff96d9d726e
Miguel Ángel H <mangelhoyos101@gmail.com>
2024-03-19 19:27:34 -0500

    Light estimation and occlusion

---
+++ Capibara AR/Assets/_Assets/Scripts/AR/ARLightEstimationHandler.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARLightEstimationHandler : MonoBehaviour
+{
+    [Header("Light estimation setup")]
+    [SerializeField] private ARCameraManager arCameraManager;
+    [SerializeField] private Light mainDirectionalLight;
+
+    private void OnEnable()
+    {
+        arCameraManager.frameReceived += LumenUpdate;
+    }
+
+    private void OnDisable()
+    {
+        arCameraManager.frameReceived -= LumenUpdate;
+    }
+
+    private void LumenUpdate(ARCameraFrameEventArgs eventData)
+    {
+        if(eventData.lightEstimation.averageBrightness.HasValue)
+            mainDirectionalLight.intensity = eventData.lightEstimation.averageBrightness.Value;
+
+        if(eventData.lightEstimation.colorCorrection.HasValue)
+            mainDirectionalLight.color = eventData.lightEstimation.colorCorrection.Value;
+    }
+}
