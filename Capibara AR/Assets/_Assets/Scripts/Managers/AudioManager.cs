f2fe1720b3a122ba31dbe91e69e7823e6ee2ecc4
Miguel Ángel H <mangelhoyos101@gmail.com>
2024-03-24 22:21:30 -0500

    Audiomanager

---
+++ Capibara AR/Assets/_Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,98 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour
+{
+
+	public static AudioManager instance;
+
+	public AudioMixerGroup mixerGroup;
+
+	public Sound[] sounds;
+
+	void Awake()
+	{
+		if (instance != null)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			instance = this;
+		}
+
+		foreach (Sound s in sounds)
+		{
+			s.source = gameObject.AddComponent<AudioSource>();
+			s.source.clip = s.clip;
+			s.source.loop = s.loop;
+
+			s.source.outputAudioMixerGroup = mixerGroup;
+		}
+	}
+
+	public void Play(string sound)
+	{
+		Sound s = Array.Find(sounds, item => item.name == sound);
+		if (s == null)
+		{
+			Debug.LogWarning("Sound: " + name + " not found!");
+			return;
+		}
+
+		s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volumeVariance / 2f, s.volumeVariance / 2f));
+		s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
+
+		s.source.Play();
+	}
+
+	public void Stop(string sound)
+	{
+		Sound s = Array.Find(sounds, item => item.name == sound);
+		if (s == null)
+		{
+			Debug.LogWarning("Sound: " + name + " not found!");
+			return;
+		}
+		
+		s.source.Stop();
+	}
+
+
+	public void SetWithFade(string sound, float fadeDuration, bool playSong)
+	{
+        Sound s = Array.Find(sounds, item => item.name == sound);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+
+		StartCoroutine(FadeCoroutine(playSong ? s.volume : 0, fadeDuration, playSong, s));
+    }
+
+	IEnumerator FadeCoroutine(float endValue,  float duration, bool isPlay, Sound sound)
+	{
+		if(isPlay)
+		{
+			sound.source.volume = 0;
+			sound.source.Play();
+		}
+
+        float elapsedTime = 0;
+
+        while (elapsedTime < duration)
+        {
+			sound.source.volume = Mathf.Lerp(sound.source.volume, endValue, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+		if (!isPlay)
+			sound.source.Stop();
+
+    }
+
+}
